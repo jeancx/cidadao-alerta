9cc7ff31092886be7b7fe50a0c4c178c45f3815e
Jean Carlos Farias <contato@jeancf.com.br>
2019-07-03 19:36:28 -0300

    Update firebase.config.js

--- dashboard/src/config/firebase.config.js
+++ dashboard/src/config/firebase.config.js
@@ -1,9 +1,9 @@
 export default {
-  apiKey: 'AIzaSyBULxy_m_iwChlmd8gY9U25mVgoe9_X_NU',
-  authDomain: 'cidadao-alerta-2019.firebaseapp.com',
-  databaseURL: 'https://cidadao-alerta-2019.firebaseio.com',
+  apiKey: 'apiKey',
+  authDomain: 'authDomain',
+  databaseURL: 'databaseURL',
   projectId: 'cidadao-alerta-2019',
   storageBucket: 'cidadao-alerta-2019.appspot.com',
-  messagingSenderId: '288394397582',
+  messagingSenderId: 'messagingSenderId',
   debug: true
 }
